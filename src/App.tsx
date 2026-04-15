38558f67cfc7c81e4cc153ddeec1120a3574ee5d
h4nsee <h4nsee@gmail.com>
2026-04-15 20:22:25 +0300

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div style={{
+      width: "100vw",
+      height: "100vh",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      background: "transparent",
+      color: "white",
+      fontSize: "32px"
+    }}>
+      Гейб работает 👀
+    </div>
+  );
+}
